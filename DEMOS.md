af469683c85da5f42a4077169eac3eea18336294
Erkal <erkal@diesdas.digital>
2021-03-20 17:46:19 +0100

    -

--- DEMOS.md
+++ DEMOS.md
@@ -9,6 +9,7 @@
 - multiple-shadows-from-ianmackenzie: [demo](https://erkal.surge.sh/multiple-shadows-from-ianmackenzie), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/multiple-shadows-from-ianmackenzie/src)
 - parameterized-curve: [demo](https://erkal.surge.sh/parameterized-curve), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/parameterized-curve/src)
 - recursive-tree: [demo](https://erkal.surge.sh/recursive-tree), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/recursive-tree/src)
+- rhombitrihexagonal-tiling: [demo](https://erkal.surge.sh/rhombitrihexagonal-tiling), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/rhombitrihexagonal-tiling/src)
 - rotating-blocks: [demo](https://erkal.surge.sh/rotating-blocks), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/rotating-blocks/src)
 - template-with-configurations: [demo](https://erkal.surge.sh/template-with-configurations), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/template-with-configurations/src)
 - template: [demo](https://erkal.surge.sh/template), [src](https://github.com/erkal/elm-3d-playground-exploration/tree/main/games/template/src)
--- games/rhombitrihexagonal-tiling/src/Main.elm
+++ games/rhombitrihexagonal-tiling/src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (main)
 
-import Color exposing (hsl, red, white)
+import Color exposing (black, blue, hsl, white)
 import Html exposing (Html)
-import Playground3d exposing (Computer, Shape, block, configurations, cube, gameWithConfigurations, getFloat, group, moveX, moveY, moveZ, rotateY, rotateZ, triangle, wave)
+import Playground3d exposing (Computer, Shape, block, configurations, cube, gameWithConfigurations, getFloat, group, moveX, moveY, moveZ, rotateX, rotateY, rotateZ, scale, spin, triangle, wave)
 import Playground3d.Camera exposing (Camera, perspective)
 import Playground3d.Scene as Scene
 
@@ -21,9 +21,9 @@ type alias Model =
 
 initialConfigurations =
     configurations
-        [ ( "hue", ( 0, 0.8, 1 ) )
-        , ( "saturation", ( 0, 0.8, 1 ) )
-        , ( "lightness", ( 0, 0.7, 1 ) )
+        [ ( "camera x", ( -40, 0, 40 ) )
+        , ( "camera y", ( -40, 0, 40 ) )
+        , ( "camera z", ( -40, 16, 40 ) )
         ]
 
 
@@ -52,33 +52,102 @@ view computer model =
         , camera =
             perspective
                 { focalPoint = { x = 0, y = 0, z = 0 }
-                , eyePoint = { x = 1, y = 3, z = 4 }
+                , eyePoint =
+                    { x = getFloat "camera x" computer
+                    , y = getFloat "camera y" computer
+                    , z = getFloat "camera z" computer
+                    }
                 , upDirection = { x = 0, y = 1, z = 0 }
                 }
         , backgroundColor = white
         , sunlightAzimuth = -(degrees 135)
         , sunlightElevation = -(degrees 45)
         }
-        [ triangleBlock computer
-            |> moveY (1 / 2 + sqrt 3 / 6)
-        , squareBlock computer
+        [ block white ( 40, 40, 1 ) |> moveZ -4
+        , allShapes computer |> moveZ -0.5
         ]
+
+
+makeUnit : Computer -> ( Int, Int ) -> Shape
+makeUnit computer ( x, y ) =
+    let
+        twoBlock =
+            group
+                [ triangleBlock computer
+                    |> moveY (1 / 2 + sqrt 3 / 6)
+                    |> rotateZ (degrees 90)
+                , squareBlock computer
+                ]
+
+        rotateAroundHexagonCenter angle shape =
+            shape
+                |> moveY -((1 + sqrt 3) / 2)
+                |> rotateZ angle
+                |> moveY ((1 + sqrt 3) / 2)
+
+        squareBlockOnRight =
+            squareBlock computer
+                |> moveY ((1 + sqrt 3) / 2)
+                |> moveX ((3 + sqrt 3) / 2)
+
+        squareBlockOnLeft =
+            squareBlock computer
+                |> rotateAroundHexagonCenter (degrees 240)
+
+        rotDegree =
+            2 * pi
+    in
+    group
+        [ twoBlock
+        , twoBlock |> rotateAroundHexagonCenter (degrees 60)
+        , twoBlock |> rotateAroundHexagonCenter (degrees 120)
+        , twoBlock |> rotateAroundHexagonCenter -(degrees 60)
+        , squareBlockOnRight
+        , squareBlockOnLeft
+        ]
+        |> rotateX (wave -rotDegree rotDegree (4 * rotDegree) computer.time)
+        |> rotateY (wave -rotDegree rotDegree (4 * rotDegree) computer.time)
+        |> moveX (toFloat x * (3 + sqrt 3))
+        |> moveY (toFloat y * (1 + sqrt 3))
+        |> moveZ (wave -1 1 7 computer.time)
+
+
+allShapes : Computer -> Shape
+allShapes computer =
+    group
+        (cartesianProduct
+            (List.range -2 2)
+            (List.range -1 1)
+            |> List.map (makeUnit computer)
+        )
+
+
+cartesianProduct : List a -> List b -> List ( b, a )
+cartesianProduct columns =
+    let
+        row i =
+            List.map (\j -> ( i, j )) columns
+    in
+    List.concatMap row
 
 
 squareBlock : Computer -> Shape
 squareBlock computer =
-    cube white 1
+    cube (color computer) 1
 
 
-
---|> moveX 0.5
+color computer =
+    hsl
+        (wave 0 1 7 computer.time)
+        0.8
+        0.8
 
 
 triangleBlock : Computer -> Shape
 triangleBlock computer =
     let
         t =
-            triangle white
+            triangle (color computer)
                 ( { x = 0, y = 0, z = 0 }
                 , { x = cos (degrees 30), y = sin (degrees 30), z = 0 }
                 , { x = 0, y = 1, z = 0 }
@@ -88,7 +157,7 @@ triangleBlock computer =
                 |> moveY -(sqrt 3 / 6)
 
         side =
-            block white ( 1, 0.00000001, 1 )
+            block (color computer) ( 1, 0.00000001, 1 )
                 |> moveY -(sqrt 3 / 6)
     in
     group
