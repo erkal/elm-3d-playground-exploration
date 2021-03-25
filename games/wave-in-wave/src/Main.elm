787d81628b9f402c0757ef836c1876df07eeca89
Erkal <erkal@diesdas.digital>
2021-03-25 22:40:40 +0100

    shiny!

--- games/wave-in-wave/src/Main.elm
+++ games/wave-in-wave/src/Main.elm
@@ -1,14 +1,20 @@
 module Main exposing (main)
 
-import Color exposing (black, blue, green, hsl, red, white)
+import Color exposing (black, blue, darkGray, gray, green, hsl, lightBlue, red, white)
 import Html exposing (Html)
-import Playground3d exposing (Computer, Shape, block, game, group, line, moveX, moveY, moveZ, rotateAround, scaleAround, sphere, wave)
+import Illuminance
+import LuminousFlux
+import Playground3d exposing (Computer, Shape, block, configurations, gameWithConfigurations, getFloat, group, line, moveX, moveY, moveZ, rotateAround, rotateY, scaleAround, wave, waveWithDelay)
 import Playground3d.Camera exposing (Camera, perspective)
+import Playground3d.Light as Light
 import Playground3d.Scene as Scene
+import Scene3d
+import Scene3d.Light
+import Temperature
 
 
 main =
-    game view update initialModel
+    gameWithConfigurations view update initialConfigurations initialModel
 
 
 type alias Model =
@@ -22,6 +28,16 @@ type alias Model =
 initialModel : Model
 initialModel =
     {}
+
+
+initialConfigurations =
+    configurations
+        [ ( "number of blocks", ( 10, 40, 60 ) )
+        , ( "frequency", ( 1, 4, 20 ) )
+        , ( "minWidth", ( 0, 5, 35 ) )
+        , ( "maxWidth", ( 10, 24, 40 ) )
+        , ( "period", ( 0.5, 7, 10 ) )
+        ]
 
 
 
@@ -39,25 +55,74 @@ update computer model =
 
 view : Computer -> Model -> Html Never
 view computer model =
-    Scene.sunny
-        { devicePixelRatio = computer.devicePixelRatio
-        , screen = computer.screen
-        , camera =
-            perspective
-                { focalPoint = { x = 0, y = 0, z = 0 }
-                , eyePoint = { x = 0, y = 4, z = 8 }
-                , upDirection = { x = 0, y = 1, z = 0 }
+    let
+        firstLight =
+            Light.point
+                { position = { x = -45, y = 30, z = 45 }
+                , chromaticity = Scene3d.Light.incandescent
+                , intensity = LuminousFlux.lumens 6000000
                 }
-        , backgroundColor = white
-        , sunlightAzimuth = -(degrees 135)
-        , sunlightElevation = -(degrees 45)
+
+        secondLight =
+            Light.point
+                { position = { x = 45, y = -30, z = 45 }
+                , chromaticity = Scene3d.Light.fluorescent
+                , intensity = LuminousFlux.lumens 6000000
+                }
+
+        thirdLight =
+            Light.directional
+                { azimuth = getFloat "azimuth for third light" computer
+                , elevation = getFloat "elevation for third light" computer
+                , chromaticity = Scene3d.Light.colorTemperature (Temperature.kelvins 2000)
+                , intensity = Illuminance.lux 40000
+                }
+
+        fourthLight =
+            Light.soft
+                { azimuth = getFloat "azimuth for fourth light" computer
+                , elevation = getFloat "elevation for fourth light" computer
+                , chromaticity = Scene3d.Light.fluorescent
+                , intensityAbove = Illuminance.lux 40
+                , intensityBelow = Illuminance.lux 60
+                }
+    in
+    Scene.custom
+        { screen = computer.screen
+        , camera = camera computer
+        , lights =
+            Scene3d.fourLights
+                firstLight
+                secondLight
+                thirdLight
+                fourthLight
+        , clipDepth = 0.1
+        , exposure = Scene3d.exposureValue 6
+        , toneMapping = Scene3d.hableFilmicToneMapping -- See ExposureAndToneMapping.elm for details
+        , whiteBalance = Scene3d.Light.fluorescent
+        , antialiasing = Scene3d.multisampling
+        , backgroundColor = darkGray
         }
         (shapes computer model)
+
+
+camera : Computer -> Camera
+camera computer =
+    perspective
+        { focalPoint = { x = 0, y = 0, z = 0 }
+        , eyePoint =
+            { x = 10
+            , y = wave -20 20 20 computer.time
+            , z = 95
+            }
+        , upDirection = { x = 0, y = 1, z = 0 }
+        }
 
 
 shapes : Computer -> Model -> List Shape
 shapes computer model =
     [ wavingBlocks computer
+    , block gray ( 300, 1, 300 ) |> moveY -33
     , axes
     ]
 
@@ -72,9 +137,56 @@ axes =
 
 
 wavingBlocks : Computer -> Shape
-wavingBlocks computer =
+wavingBlocks ({ time } as computer) =
     let
-        color =
-            hsl (wave 0 1 10 computer.time) 0.8 0.8
+        n =
+            floor (getFloat "number of blocks" computer)
+
+        color i =
+            hsl
+                (waveWithDelay
+                    (0.004 * period * toFloat i)
+                    0
+                    1
+                    3
+                    time
+                )
+                0.65
+                0.7
+
+        dist =
+            60 / toFloat n
+
+        frequency =
+            getFloat "frequency" computer
+
+        maxWidth =
+            getFloat "maxWidth" computer
+
+        minWidth =
+            getFloat "minWidth" computer
+
+        period =
+            getFloat "period" computer
+
+        xzWaving i =
+            waveWithDelay
+                (frequency * toFloat i / toFloat n)
+                minWidth
+                maxWidth
+                period
+                time
+
+        ithBlock i =
+            block (color i)
+                ( xzWaving i
+                , 0.9 * dist
+                , xzWaving i
+                )
+                |> moveY (toFloat i * dist)
+                |> rotateY (0.1 * xzWaving i)
     in
-    block color ( 1, 1, 1 )
+    group
+        (List.range -(n // 2) (n // 2)
+            |> List.map ithBlock
+        )
