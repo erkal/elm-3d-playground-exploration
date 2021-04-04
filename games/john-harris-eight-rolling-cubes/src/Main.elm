40f9e2974aab3782135934115ce27e1b89a31532
Erkal <erkal@diesdas.digital>
2021-04-04 03:43:49 +0200

    add rolling animation

--- games/john-harris-eight-rolling-cubes/src/Main.elm
+++ games/john-harris-eight-rolling-cubes/src/Main.elm
@@ -2,9 +2,10 @@ module Main exposing (main)
 
 import Color exposing (darkBlue, lightGray, red, white)
 import Cube exposing (Axis(..), Cube(..), RedFaceDirection(..), RollDirection(..), Sign(..))
+import Ease
 import Html exposing (Html, br, div, p, text)
 import Html.Attributes exposing (style)
-import Playground3d exposing (Computer, Shape, block, configurations, gameWithConfigurations, getFloat, group, moveX, moveY, moveZ, rotateX, rotateY, rotateZ)
+import Playground3d exposing (Computer, Shape, Time, block, configurations, gameWithConfigurations, getFloat, group, moveX, moveY, moveZ, passedSecondsAfter, rotateAround, rotateX, rotateY, rotateZ)
 import Playground3d.Camera as Camera exposing (Camera, perspective)
 import Playground3d.Scene as Scene
 import World exposing (RollResult(..), World)
@@ -13,7 +14,7 @@ import World exposing (RollResult(..), World)
 
 {-
    Eight Rolling Cubes Puzzle from John Harris.
-   Indirect reference from the following book of Martin Gardner:
+   As in the following book of Martin Gardner:
    "Time Travel and Other Mathematical Bewilderments" (page 118)
 -}
 
@@ -23,8 +24,19 @@ main =
 
 
 type alias Model =
-    { world : World
+    { state : State
+    , world : World
     }
+
+
+type State
+    = NoAnimation
+    | AnimatingRoll
+        { startedAt : Time
+        , startPosition : ( Int, Int )
+        , rollDirection : RollDirection
+        , newWorld : World
+        }
 
 
 
@@ -38,13 +50,15 @@ initialConfigurations =
         , ( "camera z", ( 0, 10, 16 ) )
         , ( "sunlight azimuth", ( -pi, 2, pi ) )
         , ( "sunlight elevation", ( -pi, -2, 0 ) )
-        , ( "cube scale", ( 0.5, 0.95, 1 ) )
+        , ( "cubes side length", ( 0.5, 0.95, 1 ) )
+        , ( "duration of rolling animation", ( 0.1, 0.3, 1 ) )
         ]
 
 
 init : Computer -> Model
 init computer =
-    { world = World.init
+    { state = NoAnimation
+    , world = World.init
     }
 
 
@@ -56,17 +70,28 @@ update : Computer -> Model -> Model
 update computer model =
     model
         |> handleMouseClick computer
+        |> stopRollingAnimation computer
 
 
 handleMouseClick : Computer -> Model -> Model
 handleMouseClick computer model =
     if computer.mouse.click then
-        case Camera.mouseOverXY (camera computer) computer.screen computer.mouse of
+        case
+            Camera.mouseOverXYAtZ
+                (getFloat "cubes side length" computer)
+                (camera computer)
+                computer.screen
+                computer.mouse
+        of
             Nothing ->
                 model
 
             Just { x, y } ->
-                case model.world |> World.rollCubeAt ( round x, round y ) of
+                let
+                    startPosition =
+                        ( round x, round y )
+                in
+                case model.world |> World.rollCubeAt startPosition of
                     NoCubeThere ->
                         model
 
@@ -74,10 +99,41 @@ handleMouseClick computer model =
                         model
 
                     Roll rollDirection newWorld ->
-                        { model | world = newWorld }
+                        model
+                            |> startRollAnimation computer startPosition rollDirection newWorld
 
     else
         model
+
+
+stopRollingAnimation : Computer -> Model -> Model
+stopRollingAnimation computer model =
+    case model.state of
+        NoAnimation ->
+            model
+
+        AnimatingRoll { startedAt, newWorld } ->
+            if passedSecondsAfter startedAt computer.time > getFloat "duration of rolling animation" computer then
+                { model
+                    | state = NoAnimation
+                    , world = newWorld
+                }
+
+            else
+                model
+
+
+startRollAnimation : Computer -> ( Int, Int ) -> RollDirection -> World -> Model -> Model
+startRollAnimation computer startPosition rollDirection newWorld model =
+    { model
+        | state =
+            AnimatingRoll
+                { startedAt = computer.time
+                , startPosition = startPosition
+                , rollDirection = rollDirection
+                , newWorld = newWorld
+                }
+    }
 
 
 
@@ -163,15 +219,15 @@ drawCubes : Computer -> Model -> Shape
 drawCubes computer model =
     group
         (model.world
-            |> List.map (drawCube computer)
+            |> List.map (drawCube computer model)
         )
 
 
-drawCube : Computer -> Cube -> Shape
-drawCube computer (Cube ( x, y ) redFaceDirection) =
+drawCube : Computer -> Model -> Cube -> Shape
+drawCube computer model (Cube ( x, y ) redFaceDirection) =
     let
         s =
-            getFloat "cube scale" computer
+            getFloat "cubes side length" computer
 
         redHalf =
             block red ( s, s, s / 2 ) |> moveZ (s / 4)
@@ -179,7 +235,7 @@ drawCube computer (Cube ( x, y ) redFaceDirection) =
         blackHalf =
             block darkBlue ( s, s, s / 2 ) |> moveZ -(s / 4)
 
-        rotation =
+        positionWithRedFaceDirection =
             case redFaceDirection of
                 RedFaceDirection Z Positive ->
                     identity
@@ -203,7 +259,50 @@ drawCube computer (Cube ( x, y ) redFaceDirection) =
         [ redHalf
         , blackHalf
         ]
-        |> rotation
+        |> positionWithRedFaceDirection
+        |> rollingAnimation computer model ( x, y )
         |> moveZ (s / 2)
         |> moveX (toFloat x)
         |> moveY (toFloat y)
+
+
+rollingAnimation : Computer -> Model -> ( Int, Int ) -> (Shape -> Shape)
+rollingAnimation computer model pos =
+    case model.state of
+        NoAnimation ->
+            identity
+
+        AnimatingRoll { startedAt, startPosition, rollDirection, newWorld } ->
+            if startPosition == pos then
+                let
+                    axis =
+                        case rollDirection of
+                            Up ->
+                                ( { x = 0.5, y = 0.5, z = -0.5 }, ( -1, 0, 0 ) )
+
+                            Down ->
+                                ( { x = -0.5, y = -0.5, z = -0.5 }, ( 1, 0, 0 ) )
+
+                            Left ->
+                                ( { x = -0.5, y = 0.5, z = -0.5 }, ( 0, -1, 0 ) )
+
+                            Right ->
+                                ( { x = 0.5, y = -0.5, z = -0.5 }, ( 0, 1, 0 ) )
+
+                    duration =
+                        getFloat "duration of rolling animation" computer
+
+                    passedDurationRatio =
+                        passedSecondsAfter startedAt computer.time / duration
+
+                    easedDurationRatio =
+                        Ease.inOutSine passedDurationRatio
+
+                    rotationDegree =
+                        (easedDurationRatio * degrees 90)
+                            |> min (degrees 90)
+                in
+                rotateAround axis rotationDegree
+
+            else
+                identity
