e629ea48cdf9c3ff68c83b6acdbb4b67a6b944cd
Erkal <erkal@diesdas.digital>
2021-06-14 02:22:11 +0200

    improve golden shower example

--- games/gold-shower/src/Main.elm
+++ games/gold-shower/src/Main.elm
@@ -21,10 +21,11 @@ type alias Model =
 
 initialConfigurations =
     configurations
-        [ ( "camera distance", ( 3, 20, 60 ) )
+        [ ( "camera distance", ( 3, 50, 60 ) )
         , ( "camera azimuth", ( 0, 0, 2 * pi ) )
         , ( "camera elevation", ( -pi / 2, 0.5, pi / 2 ) )
-        , ( "delay", ( 0, 0.5, 1 ) )
+        , ( "delay", ( 0, 0.04, 1 ) )
+        , ( "number of cubes", ( 10, 40, 100 ) )
         ]
         []
 
@@ -77,22 +78,31 @@ makeCube computer i =
     let
         delay =
             getFloat "delay" computer
+
+        n =
+            floor (getFloat "number of cubes" computer)
+
+        size =
+            30 / toFloat n
     in
-    cube yellow 1
+    cube yellow size
         |> rotateZ (Playground3d.waveWithDelay (delay * toFloat i) 1 3 4 computer.time)
-        |> scale (Playground3d.waveWithDelay (delay * toFloat i) 1 3 4 computer.time)
-        |> moveX 3
+        |> scale (Playground3d.waveWithDelay (delay * toFloat i) 1 6 4 computer.time)
+        |> moveX (Playground3d.waveWithDelay (delay * toFloat i) 1 3 1 computer.time)
         |> rotateY (Playground3d.waveWithDelay (delay * toFloat i) 0 10 20 computer.time)
-        |> moveY (1.1 * toFloat i)
+        |> moveY (size * 1.1 * toFloat i)
 
 
 cubes : Computer -> Shape
 cubes computer =
+    let
+        n =
+            floor (getFloat "number of cubes" computer)
+    in
     group
-        (List.range 0 18
+        (List.range -(n // 2) (n // 2)
             |> List.map (makeCube computer)
         )
-        |> moveY -10
 
 
 
