966a66bd606775b16e0934ea4988855eb962f351
Erkal <erkal@diesdas.digital>
2021-06-23 05:14:16 +0200

    create module Computer.elm

--- 3d-playground/src/Playground3d.elm
+++ 3d-playground/src/Playground3d.elm
@@ -1,12 +1,11 @@
 port module Playground3d exposing
     ( game
     , Shape, block, cube, cylinder, group, line, sphere, triangle
-    , move, moveX, moveY, moveZ, rotateX, rotateY, rotateZ
+    , move, moveX, moveY, moveZ, rotateX, rotateY, rotateZ, rotateAround, scale, scaleAround
     , spin, wave, zigzag
     , Computer, Mouse, Screen, Keyboard, toX, toY, toXY
-    , Number
     , toEntities
-    , configurations, gameWithConfigurations, gameWithConfigurationsAndEditor, getColor, getFloat, rotateAround, scale, scaleAround
+    , configurations, gameWithConfigurations, gameWithConfigurationsAndEditor, getColor, getFloat
     )
 
 {-| NOTE: Most of the following code is copied from evancz/elm-playground
@@ -24,7 +23,7 @@ port module Playground3d exposing
 
 # Move Shapes
 
-@docs move, moveX, moveY, moveZ, rotateX, rotateY, rotateZ
+@docs move, moveX, moveY, moveZ, rotateX, rotateY, rotateZ, rotateAround, scale, scaleAround
 
 
 # Groups
@@ -42,11 +41,6 @@ port module Playground3d exposing
 @docs Computer, Mouse, Screen, Keyboard, toX, toY, toXY
 
 
-### Numbers
-
-@docs Number
-
-
 ### Rendering
 
 @docs toEntities
@@ -60,6 +54,7 @@ import Browser
 import Browser.Dom as Dom
 import Browser.Events as E
 import Color exposing (Color)
+import Computer exposing (Computer, emptyKeyboard, mouseClick, mouseDown, mouseMove, toScreen, updateKeyboard)
 import Cylinder3d exposing (Cylinder3d)
 import Dict exposing (Dict)
 import Direction3d exposing (Direction3d)
@@ -75,7 +70,6 @@ import Playground3d.Geometry exposing (Point)
 import Point3d exposing (Point3d)
 import Scene3d
 import Scene3d.Material as Material exposing (Material)
-import Set exposing (Set)
 import Sphere3d exposing (Sphere3d)
 import Task
 import Triangle3d exposing (Triangle3d)
@@ -106,300 +100,47 @@ type alias TouchEvent =
 
 
 
--- COMPUTER
+-- EXPORTS
 
 
-{-| When writing a [`game`](#game), you can look up all sorts of information
-about your computer:
-
-  - [`Mouse`](#Mouse) - Where is the mouse right now?
-  - [`Keyboard`](#Keyboard) - Are the arrow keys down?
-  - [`Screen`](#Screen) - How wide is the screen?
-  - [`Time`](#Time) - What time is it right now in seconds, starting from 0?
-
-So you can use expressions like `computer.mouse.x` and `computer.keyboard.enter`
-in games where you want some mouse or keyboard interaction.
-
--}
 type alias Computer =
-    { mouse : Mouse
-    , touches : Dict Int { x : Float, y : Float }
-    , keyboard : Keyboard
-    , screen : Screen
-    , time : Float
-    , configurations : Configurations
-    , devicePixelRatio : Float
-    }
+    Computer.Computer
+
+
+type alias Mouse =
+    Computer.Mouse
+
+
+type alias Screen =
+    Computer.Screen
+
+
+type alias Keyboard =
+    Computer.Keyboard
+
+
+toX =
+    Computer.toX
+
+
+toY =
+    Computer.toX
+
+
+toXY =
+    Computer.toXY
 
 
 configurations =
-    Configurations.init
+    Computer.configurations
 
 
-getFloat : String -> Computer -> Float
-getFloat key computer =
-    computer.configurations |> Configurations.getFloat key
+getColor =
+    Computer.getColor
 
 
-getColor : String -> Computer -> Color
-getColor key computer =
-    computer.configurations |> Configurations.getColor key
-
-
-
--- MOUSE
-
-
-{-| Figure out what is going on with the mouse.
-
-You could draw a circle around the mouse with a program like this:
-
-    import Playground3d exposing (..)
-
-    main =
-        game view update 0
-
-    view computer gameModel =
-        [ circle yellow 40
-            |> moveX computer.mouse.x
-            |> moveY computer.mouse.y
-        ]
-
-    update computer gameModel =
-        gameModel
-
-You could also use `computer.mouse.down` to change the color of the circle
-while the mouse button is down.
-
--}
-type alias Mouse =
-    { x : Number
-    , y : Number
-    , down : Bool
-    , click : Bool
-    }
-
-
-{-| A number like `1` or `3.14` or `-120`.
--}
-type alias Number =
-    Float
-
-
-
--- KEYBOARD
-
-
-{-| Figure out what is going on with the keyboard.
-
-If someone is pressing the UP and RIGHT arrows, you will see a value like this:
-
-    { up = True
-    , down = False
-    , left = False
-    , right = True
-    , space = False
-    , enter = False
-    , shift = False
-    , backspace = False
-    , keys = Set.fromList [ "ArrowUp", "ArrowRight" ]
-    }
-
-So if you want to move a character based on arrows, you could write an update
-like this:
-
-    update computer y =
-        if computer.keyboard.up then
-            y + 1
-
-        else
-            y
-
-Check out [`toX`](#toX) and [`toY`](#toY) which make this even easier!
-
-**Note:** The `keys` set will be filled with the name of all keys which are
-down right now. So you will see things like `"a"`, `"b"`, `"c"`, `"1"`, `"2"`,
-`"Space"`, and `"Control"` in there. Check out [this list][list] to see the
-names used for all the different special keys! From there, you can use
-[`Set.member`][member] to check for whichever key you want. E.g.
-`Set.member "Control" computer.keyboard.keys`.
-
-[list]: https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key/Key_Values
-[member]: /packages/elm/core/latest/Set#member
-
--}
-type alias Keyboard =
-    { up : Bool
-    , down : Bool
-    , left : Bool
-    , right : Bool
-    , space : Bool
-    , enter : Bool
-    , shift : Bool
-    , backspace : Bool
-    , keys : Set.Set String
-    }
-
-
-{-| Turn the LEFT and RIGHT arrows into a number.
-
-    toX { left = False, right = False, ... } == 0
-    toX { left = True , right = False, ... } == -1
-    toX { left = False, right = True , ... } == 1
-    toX { left = True , right = True , ... } == 0
-
-So to make a square move left and right based on the arrow keys, we could say:
-
-    import Playground3d exposing (..)
-
-    main =
-        game view update 0
-
-    view computer x =
-        [ square green 40
-            |> moveX x
-        ]
-
-    update computer x =
-        x + toX computer.keyboard
-
--}
-toX : Keyboard -> Number
-toX keyboard =
-    (if keyboard.right then
-        1
-
-     else
-        0
-    )
-        - (if keyboard.left then
-            1
-
-           else
-            0
-          )
-
-
-{-| Turn the UP and DOWN arrows into a number.
-
-    toY { up = False, down = False, ... } == 0
-    toY { up = True , down = False, ... } == 1
-    toY { up = False, down = True , ... } == -1
-    toY { up = True , down = True , ... } == 0
-
-This can be used to move characters around in games just like [`toX`](#toX):
-
-    import Playground3d exposing (..)
-
-    main =
-        game view update ( 0, 0 )
-
-    view computer ( x, y ) =
-        [ square blue 40
-            |> move x y
-        ]
-
-    update computer ( x, y ) =
-        ( x + toX computer.keyboard
-        , y + toY computer.keyboard
-        )
-
--}
-toY : Keyboard -> Number
-toY keyboard =
-    (if keyboard.up then
-        1
-
-     else
-        0
-    )
-        - (if keyboard.down then
-            1
-
-           else
-            0
-          )
-
-
-{-| If you just use `toX` and `toY`, you will move diagonal too fast. You will go
-right at 1 pixel per update, but you will go up/right at 1.41421 pixels per
-update.
-
-So `toXY` turns the arrow keys into an `(x,y)` pair such that the distance is
-normalized:
-
-    toXY { up = True , down = False, left = False, right = False, ... } == (1, 0)
-    toXY { up = True , down = False, left = False, right = True , ... } == (0.707, 0.707)
-    toXY { up = False, down = False, left = False, right = True , ... } == (0, 1)
-
-Now when you go up/right, you are still going 1 pixel per update.
-
-    import Playground3d exposing (..)
-
-    main =
-        game view update ( 0, 0 )
-
-    view computer ( x, y ) =
-        [ square green 40
-            |> move x y
-        ]
-
-    update computer ( x, y ) =
-        let
-            ( dx, dy ) =
-                toXY computer.keyboard
-        in
-        ( x + dx, y + dy )
-
--}
-toXY : Keyboard -> ( Number, Number )
-toXY keyboard =
-    let
-        x =
-            toX keyboard
-
-        y =
-            toY keyboard
-    in
-    if x /= 0 && y /= 0 then
-        ( x / squareRootOfTwo, y / squareRootOfTwo )
-
-    else
-        ( x, y )
-
-
-squareRootOfTwo : Number
-squareRootOfTwo =
-    sqrt 2
-
-
-
--- SCREEN
-
-
-{-| Get the dimensions of the screen. If the screen is 800 by 600, you will see
-a value like this:
-
-    { width = 800
-    , height = 600
-    , top = 300
-    , left = -400
-    , right = 400
-    , bottom = -300
-    }
-
-This can be nice when used with [`moveY`](#moveY) if you want to put something
-on the bottom of the screen, no matter the dimensions.
-
--}
-type alias Screen =
-    { width : Number
-    , height : Number
-    , top : Number
-    , left : Number
-    , right : Number
-    , bottom : Number
-    }
+getFloat =
+    Computer.getFloat
 
 
 
@@ -434,7 +175,7 @@ It will do a full rotation once every eight seconds. Try changing the `8` to
 a `2` to make it do a full rotation every two seconds. It moves a lot faster!
 
 -}
-spin : Number -> Time -> Number
+spin : Float -> Time -> Float
 spin period time =
     360 * toFrac period time
 
@@ -456,7 +197,7 @@ The radius of the circle will cycles between 50 and 90 every seven seconds.
 It kind of looks like it is breathing.
 
 -}
-wave : Number -> Number -> Number -> Time -> Number
+wave : Float -> Float -> Float -> Time -> Float
 wave lo hi period time =
     lo + (hi - lo) * (1 + cos (turns (toFrac period time))) / 2
 
@@ -479,7 +220,7 @@ It gets rotated by an angle. The angle cycles from -20 degrees to 20 degrees
 every four seconds.
 
 -}
-zigzag : Number -> Number -> Number -> Time -> Number
+zigzag : Float -> Float -> Float -> Time -> Float
 zigzag lo hi period time =
     lo + (hi - lo) * abs (2 * toFrac period time - 1)
 
@@ -629,7 +370,7 @@ gameWithConfigurationsAndEditor viewGameModel updateGameModel initialConfigurati
 
 initialComputer : Flags -> Configurations -> Computer
 initialComputer flags initialConfigurations =
-    { mouse = Mouse 0 0 False False
+    { mouse = Computer.Mouse 0 0 False False
     , touches = Dict.empty
     , keyboard = emptyKeyboard
     , screen = toScreen 600 600
@@ -951,100 +692,9 @@ gameUpdate updateGameModel updateFromEditor msg ({ computer } as model) =
                 | computer =
                     { computer
                         | keyboard = emptyKeyboard
-                        , mouse = Mouse computer.mouse.x computer.mouse.y False False
+                        , mouse = Computer.Mouse computer.mouse.x computer.mouse.y False False
                     }
             }
-
-
-
--- SCREEN HELPERS
-
-
-toScreen : Float -> Float -> Screen
-toScreen width height =
-    { width = width
-    , height = height
-    , top = height / 2
-    , left = -width / 2
-    , right = width / 2
-    , bottom = -height / 2
-    }
-
-
-
--- MOUSE HELPERS
-
-
-mouseClick : Bool -> Mouse -> Mouse
-mouseClick bool mouse =
-    { mouse | click = bool }
-
-
-mouseDown : Bool -> Mouse -> Mouse
-mouseDown bool mouse =
-    { mouse | down = bool }
-
-
-mouseMove : Float -> Float -> Mouse -> Mouse
-mouseMove x y mouse =
-    { mouse | x = x, y = y }
-
-
-
--- KEYBOARD HELPERS
-
-
-emptyKeyboard : Keyboard
-emptyKeyboard =
-    { up = False
-    , down = False
-    , left = False
-    , right = False
-    , space = False
-    , enter = False
-    , shift = False
-    , backspace = False
-    , keys = Set.empty
-    }
-
-
-updateKeyboard : Bool -> String -> Keyboard -> Keyboard
-updateKeyboard isDown key keyboard =
-    let
-        keys =
-            if isDown then
-                Set.insert key keyboard.keys
-
-            else
-                Set.remove key keyboard.keys
-    in
-    case key of
-        " " ->
-            { keyboard | keys = keys, space = isDown }
-
-        "Enter" ->
-            { keyboard | keys = keys, enter = isDown }
-
-        "Shift" ->
-            { keyboard | keys = keys, shift = isDown }
-
-        "Backspace" ->
-            { keyboard | keys = keys, backspace = isDown }
-
-        "ArrowUp" ->
-            { keyboard | keys = keys, up = isDown }
-
-        "ArrowDown" ->
-            { keyboard | keys = keys, down = isDown }
-
-        "ArrowLeft" ->
-            { keyboard | keys = keys, left = isDown }
-
-        "ArrowRight" ->
-            { keyboard | keys = keys, right = isDown }
-
-        _ ->
-            { keyboard | keys = keys }
 
 
 
