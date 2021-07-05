13567bbd266cdf5a83322b51965e95732027ed91
Erkal <erkal@diesdas.digital>
2021-07-05 18:28:28 +0200

    03

--- examples/isomorphism-game/src/Main.elm
+++ examples/isomorphism-game/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
-import Color exposing (hsl, rgb, rgb255)
-import Graph exposing (Graph, VertexData, VertexId)
+import Color exposing (blue, green, hsl, red, rgb, rgb255)
+import Graph exposing (Graph, Point, VertexData, VertexId)
 import Html exposing (Html)
 import Playground3d exposing (Computer, colorConfig, floatConfig, gameWithConfigurations, getColor, getFloat)
 import Playground3d.Animation exposing (..)
@@ -25,8 +25,9 @@ type alias Model =
 initialConfigurations =
     [ floatConfig "camera distance" ( 3, 40 ) 20
     , floatConfig "camera azimuth" ( 0, 2 * pi ) 0
-    , floatConfig "camera elevation" ( -pi / 2, pi / 2 ) 0.5
-    , colorConfig "cube" (rgb255 59 232 203)
+    , floatConfig "camera elevation" ( -pi / 2, pi / 2 ) -0.5
+    , colorConfig "vertex" (rgb255 59 232 203)
+    , colorConfig "edge" (rgb255 59 232 203)
     , colorConfig "floor" (rgb255 35 118 139)
     , floatConfig "period" ( 0.1, 5 ) 1
     ]
@@ -73,13 +74,37 @@ view computer model =
         }
         [ drawFloor computer
         , drawVertices computer model
+        , drawEdge computer ( Point 0 0, Point 1 1 )
+        , axes
         ]
+
+
+axes : Shape
+axes =
+    group
+        [ line red ( 100, 0, 0 ) -- x axis
+        , line green ( 0, 100, 0 ) -- y axis
+        , line blue ( 0, 0, 100 ) -- z axis
+        ]
+
+
+drawEdge : Computer -> ( Point, Point ) -> Shape
+drawEdge computer ( start_, end ) =
+    let
+        ( length, rotation ) =
+            toPolar ( end.x - start_.x, end.y - start_.y )
+    in
+    block (getColor "edge" computer) ( length, 0.3, 0.3 )
+        |> moveX (length / 2)
+        |> rotateZ rotation
+        |> moveX start_.x
+        |> moveY start_.y
 
 
 drawFloor : Computer -> Shape
 drawFloor computer =
-    block (getColor "floor" computer) ( 10, 1, 10 )
-        |> moveY -0.5
+    block (getColor "floor" computer) ( 10, 10, 1 )
+        |> moveZ -0.5
 
 
 drawVertices : Computer -> Model -> Shape
@@ -92,7 +117,8 @@ drawVertices computer model =
 
 drawVertex : Computer -> ( VertexId, VertexData ) -> Shape
 drawVertex computer ( _, { position } ) =
-    cylinder (getColor "cube" computer) 0.5 0.4
+    cylinder (getColor "vertex" computer) 0.5 0.4
+        |> rotateX (degrees 90)
         |> scale (wave 1 1.1 1 computer.time)
         |> moveX position.x
-        |> moveZ position.y
+        |> moveY position.y
