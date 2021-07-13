554dd00c2b939e6551a3791ffeec75ee96b66bf2
Erkal <erkal@diesdas.digital>
2021-07-14 00:16:58 +0200

    -

--- examples/wave-in-wave/src/Main.elm
+++ examples/wave-in-wave/src/Main.elm
@@ -91,7 +91,8 @@ view computer model =
                 }
     in
     Scene.custom
-        { screen = computer.screen
+        { devicePixelRatio = computer.devicePixelRatio
+        , screen = computer.screen
         , camera = camera computer
         , lights =
             Scene3d.fourLights
