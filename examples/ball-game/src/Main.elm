dba6dbf17bf6d393cc2885be55af7ce10ff558e4
Erkal <erkal@diesdas.digital>
2021-08-06 20:05:31 +0200

    -

--- examples/ball-game/src/Main.elm
+++ examples/ball-game/src/Main.elm
@@ -1,10 +1,17 @@
 module Main exposing (main)
 
 import Camera exposing (Camera, perspectiveWithOrbit)
-import Color exposing (blue, gray, green, hsl, orange, red, rgb255)
+import Color exposing (black, blue, gray, green, hsl, lightBlue, lightGray, orange, red, rgb255, white)
+import Geometry exposing (Point, Vector, addVector, scaleBy, translateBy)
 import Html exposing (Html)
-import Playground exposing (Computer, colorConfig, configBlock, floatConfig, gameWithConfigurations, getColor, getFloat, toX, toY)
+import Illuminance
+import LuminousFlux
+import Playground exposing (Computer, colorConfig, configBlock, floatConfig, gameWithConfigurations, getColor, getFloat, toX, toXY, toY)
+import Playground.Light as Light
 import Scene exposing (..)
+import Scene3d
+import Scene3d.Light
+import Temperature
 
 
 main =
@@ -12,10 +19,21 @@ main =
 
 
 type alias Model =
-    { x : Float
-    , z : Float
-    , vx : Float
-    , vz : Float
+    Ball
+
+
+type alias Ball =
+    { position : Point
+    , speed : Vector
+    , acceleration : Vector
+    }
+
+
+initialBall : Ball
+initialBall =
+    { position = Point 0 0 0
+    , speed = ( 0, 0, 0 )
+    , acceleration = ( 0, 0, 0 )
     }
 
 
@@ -28,22 +46,18 @@ initialConfigurations =
         True
         [ floatConfig "camera distance" ( 3, 60 ) 20
         , floatConfig "camera azimuth" ( 0, 2 * pi ) 0
-        , floatConfig "camera elevation" ( -pi / 2, pi / 2 ) (pi / 2)
+        , floatConfig "camera elevation" ( -pi / 2, pi / 2 ) 0.6
         ]
     , configBlock "Color"
         True
-        [ colorConfig "cube color" (hsl 0 0.36 0.5)
+        [ colorConfig "floor color" white
         ]
     ]
 
 
 init : Computer -> Model
 init computer =
-    { x = 0
-    , z = 0
-    , vx = 0
-    , vz = 0
-    }
+    initialBall
 
 
 
@@ -53,23 +67,41 @@ init computer =
 update : Computer -> Model -> Model
 update computer model =
     model
-        |> handlePlayerInput computer
+        |> handleArrowKeys computer
+        |> limitBall computer
         |> physics computer
 
 
-handlePlayerInput : Computer -> Model -> Model
-handlePlayerInput computer model =
+limitBall : Computer -> Model -> Model
+limitBall computer model =
     { model
-        | vx = model.vx + 0.05 * toX computer.keyboard
-        , vz = model.vz - 0.05 * toY computer.keyboard
+        | position =
+            Point
+                (model.position.x |> clamp -4 4)
+                model.position.y
+                (model.position.z |> clamp -4 4)
+    }
+
+
+handleArrowKeys : Computer -> Model -> Model
+handleArrowKeys computer model =
+    let
+        force =
+            ( toX computer.keyboard
+            , 0
+            , -(toY computer.keyboard)
+            )
+    in
+    { model
+        | speed =
+            model.speed |> addVector (scaleBy 0.1 force)
     }
 
 
 physics : Computer -> Model -> Model
 physics computer model =
     { model
-        | x = model.x + 0.16 * model.vx
-        , z = model.z + 0.16 * model.vz
+        | position = model.position |> translateBy (scaleBy 0.16 model.speed)
     }
 
 
@@ -89,13 +121,59 @@ camera computer =
 
 view : Computer -> Model -> Html Never
 view computer model =
-    Scene.sunny
+    viewGame computer model
+
+
+viewGame : Computer -> Model -> Html Never
+viewGame computer model =
+    let
+        firstLight =
+            Light.point
+                { position = { x = -2, y = 4, z = 1 }
+                , chromaticity = Scene3d.Light.incandescent
+                , intensity = LuminousFlux.lumens 6000
+                }
+
+        secondLight =
+            Light.point
+                { position = { x = 2, y = 3, z = 1 }
+                , chromaticity = Scene3d.Light.fluorescent
+                , intensity = LuminousFlux.lumens 6000
+                }
+
+        thirdLight =
+            Light.directional
+                { azimuth = getFloat "azimuth for third light" computer
+                , elevation = getFloat "elevation for third light" computer
+                , chromaticity = Scene3d.Light.colorTemperature (Temperature.kelvins 2000)
+                , intensity = Illuminance.lux 120
+                }
+
+        fourthLight =
+            Light.soft
+                { azimuth = getFloat "azimuth for fourth light" computer
+                , elevation = getFloat "elevation for fourth light" computer
+                , chromaticity = Scene3d.Light.fluorescent
+                , intensityAbove = Illuminance.lux 20
+                , intensityBelow = Illuminance.lux 10
+                }
+    in
+    Scene.custom
         { devicePixelRatio = computer.devicePixelRatio
         , screen = computer.screen
         , camera = camera computer
+        , lights =
+            Scene3d.fourLights
+                firstLight
+                secondLight
+                thirdLight
+                fourthLight
+        , clipDepth = 0.1
+        , exposure = Scene3d.exposureValue 6
+        , toneMapping = Scene3d.hableFilmicToneMapping -- See ExposureAndToneMapping.elm for details
+        , whiteBalance = Scene3d.Light.fluorescent
+        , antialiasing = Scene3d.multisampling
         , backgroundColor = rgb255 46 46 46
-        , sunlightAzimuth = -(degrees 135)
-        , sunlightElevation = -(degrees 45)
         }
         [ drawAxes
         , drawFloor computer
@@ -114,7 +192,7 @@ drawAxes =
 
 drawFloor : Computer -> Shape
 drawFloor computer =
-    block gray ( 10, 1, 10 )
+    block (getColor "floor color" computer) ( 10, 1, 10 )
         |> moveY -0.5
 
 
@@ -122,21 +200,30 @@ drawPlayer : Computer -> Model -> Shape
 drawPlayer computer model =
     let
         playerBall =
-            sphere (getColor "cube color" computer) 0.5
+            group
+                [ sphere red 0.5 |> moveX -0.01
+                , sphere black 0.5 |> moveX 0.01
+                ]
+
+        ( vx, _, vz ) =
+            model.speed
 
         ( length, rotation ) =
-            toPolar ( model.vx, model.vz )
+            toPolar ( vx, vz )
 
         speedVector =
-            cylinder orange 0.1 (2 * length)
+            cylinder red 0.2 (2 * length)
                 |> moveY length
                 |> rotateZ -(degrees 90)
                 |> rotateY -rotation
+                |> moveY 0.6
     in
     group
         [ speedVector
-        , playerBall
+        , group
+            [ playerBall
+            ]
         ]
         |> moveY 0.5
-        |> moveX model.x
-        |> moveZ model.z
+        |> moveX model.position.x
+        |> moveZ model.position.z
--- scene-3d/src/Geometry.elm
+++ scene-3d/src/Geometry.elm
@@ -1,4 +1,10 @@
-module Geometry exposing (Point, Vector, translateBy)
+module Geometry exposing
+    ( Point
+    , Vector
+    , addVector
+    , scaleBy
+    , translateBy
+    )
 
 
 type alias Point =
@@ -10,6 +16,22 @@ type alias Point =
 
 type alias Vector =
     ( Float, Float, Float )
+
+
+addVector : Vector -> Vector -> Vector
+addVector ( x1, y1, z1 ) ( x2, y2, z2 ) =
+    ( x1 + x2
+    , y1 + y2
+    , z1 + z2
+    )
+
+
+scaleBy : Float -> Vector -> Vector
+scaleBy k ( dx, dy, dz ) =
+    ( k * dx
+    , k * dy
+    , k * dz
+    )
 
 
 translateBy : Vector -> Point -> Point
