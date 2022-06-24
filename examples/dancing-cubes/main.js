b6484ae36c5db374e3df39b6d45d3a7bcf482e1f
Erkal <erkalselman@gmail.com>
2022-06-24 21:22:24 +0200

    correct all main.js files in experiments

--- examples/dancing-cubes/main.js
+++ examples/dancing-cubes/main.js
@@ -1,36 +1,12 @@
 import { Elm } from "./src/Main.elm";
+import {
+  sendInputsToElmApp,
+  initialInputs,
+} from "../../playground-exploration/src/input";
 
-const root = document.querySelector("#app div");
 const app = Elm.Main.init({
-  node: root,
-  flags: { devicePixelRatio: window.devicePixelRatio },
+  node: document.querySelector("#app div"),
+  flags: { inputs: initialInputs },
 });
 
-window.addEventListener("touchstart", (event) => {
-  app.ports.touchStart.send(getTouchEvents(event.changedTouches));
-});
-
-window.addEventListener("touchmove", (event) => {
-  app.ports.touchMove.send(getTouchEvents(event.changedTouches));
-});
-
-window.addEventListener("touchend", (event) => {
-  app.ports.touchEnd.send(getTouchEvents(event.changedTouches));
-});
-
-window.addEventListener("touchcancel", (event) => {
-  app.ports.touchCancel.send(getTouchEvents(event.changedTouches));
-});
-
-// touch
-function getTouchEvents(touches) {
-  const a = [];
-  for (let i = 0; i < touches.length; i++) {
-    a.push({
-      identifier: touches[i].identifier,
-      pageX: touches[i].pageX,
-      pageY: touches[i].pageY,
-    });
-  }
-  return a;
-}
+sendInputsToElmApp(app);
