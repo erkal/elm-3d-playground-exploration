81e16fdb46f936e575f43309dc404c089d423301
Erkal <erkalselman@gmail.com>
2022-06-24 23:26:41 +0200

    rename initialInputs to inputs

--- examples/dancing-cubes/main.js
+++ examples/dancing-cubes/main.js
@@ -1,12 +1,12 @@
 import { Elm } from "./src/Main.elm";
 import {
   sendInputsToElmApp,
-  initialInputs,
+  inputs,
 } from "../../playground-exploration/src/input";
 
 const app = Elm.Main.init({
   node: document.querySelector("#app div"),
-  flags: { inputs: initialInputs },
+  flags: { inputs: inputs },
 });
 
 sendInputsToElmApp(app);
