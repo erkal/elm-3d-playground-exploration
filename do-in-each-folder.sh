0bc92fc761e09d41a970f7d182c9e60a91e4ee04
Erkal <erkalselman@gmail.com>
2022-06-25 22:05:45 +0200

    -

--- do-in-each-folder.sh
+++ do-in-each-folder.sh
@@ -15,7 +15,11 @@ for ((i = 1; i <= ${#dirs[@]}; i++)); do
     # START
     
     # remove the directories node_modules and elm-stuff
+    rm -rf node_modules/
+    rm -rf elm-stuff/
 
+    # copy elm-tooling.json, elm.json, index.html, main.js, package.json, postcss.config.js, style.css, tailwind.config.js, vite.config.js
+    cp -fr ../_tailwind-experiment/elm-tooling.json ./
     
     # END
     
