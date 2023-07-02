a074f4c77aa37402ce074aee87a9647a338677ff
Erkal Selman <erkalselman@gmail.com>
2023-07-02 21:37:49 +0200

    successfully deploy on github pages

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -28,11 +28,6 @@ for example in examples/*; do
         cp "${example}/image.png" "build/${example_name}/image.png"
     fi
 
-    # Check if description.txt exists, if yes, copy it to the build directory
-    if [ -f "${example}/description.txt" ]; then
-        cp "${example}/description.txt" "build/${example_name}/description.txt"
-    fi
-
     # Store the inputs and output strings
     input="examples/${example_name}/Main.elm"
     output="build/${example_name}/main.js"
@@ -48,6 +43,10 @@ targets_json=${targets_json%?}
 if [ -f "elm-watch.json" ]; then
     rm "elm-watch.json"
 fi
+
+# Copy the input.js and preventEvents.js files to the build directory
+cp playground-exploration/src/input.js build/
+cp playground-exploration/src/preventEvents.js build/
 
 # Create the JSON object and write it into elm-watch.json using Node.js
 node -e "const fs = require('fs'); const data = { targets: { ${targets_json} } }; fs.writeFileSync('elm-watch.json', JSON.stringify(data, null, 2));"
